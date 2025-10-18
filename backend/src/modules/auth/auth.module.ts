bc045c9edf2e86eea67b3367e65c94c2b6901d3d
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-10-17 21:20:25 -0300

    dependencias y cors

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -3,10 +3,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants/jwt.constant';
-import { UsuarioService } from '../usuario/usuario.service';
+
+import { UsuarioModule } from '../usuario/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from '../usuario/entities/usuario.entity';
 
 @Module({
-  imports: [UsuarioService,JwtModule.register({
+  imports: [TypeOrmModule.forFeature([Usuario])
+    ,UsuarioModule,
+    JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '1d' }, // Token expiration time
